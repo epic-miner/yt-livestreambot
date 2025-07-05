b75e7787b548e703ef375d8656f0a276882451fc
epic miner <155073729+epic-miner@users.noreply.github.com>
2025-07-05 01:04:42 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import subprocess
 import telebot
 
 # Replace this with your Telegram bot token
-TELEGRAM_BOT_TOKEN = "7260616953:AAE4Ht4aVoSWm4oH-UWUGkvIROdI_cpmEMg"
+TELEGRAM_BOT_TOKEN = "8026602763:AAGwDwsj44nxRnKatGGmGOPz4guQo9fghww"
 
 # Initialize the bot
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
